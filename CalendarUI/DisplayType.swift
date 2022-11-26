3ee9a639fc19de707eb66ecb3886309d224b159e
nk-5 <nkws27@gmail.com>
2022-11-26 16:04:27 +0900

    add display type

---
+++ CalendarUI/DisplayType.swift
@@ -0,0 +1,14 @@
+//
+//  DisplayType.swift
+//  CalendarUI
+//
+//  Created by Keigo Nakagawa on 2022/11/26.
+//
+
+import Foundation
+
+public enum DisplayType {
+    case month
+    case week
+    case day
+}
